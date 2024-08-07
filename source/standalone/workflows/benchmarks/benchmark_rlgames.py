136ab03e7bf1e0d130ea1083371b0526c7a72a88
ViktorM <vmakoviychuk@nvidia.com>
2024-08-07 13:01:51 -0700

    Fixed rl benchmark.

--- source/standalone/workflows/benchmarks/benchmark_rlgames.py
+++ source/standalone/workflows/benchmarks/benchmark_rlgames.py
@@ -76,6 +76,7 @@ from omni.isaac.lab.utils.dict import print_dict
 from omni.isaac.lab.utils.io import dump_pickle, dump_yaml
 
 import omni.isaac.lab_tasks  # noqa: F401
+import humanoid.tasks  # noqa: F401
 from omni.isaac.lab_tasks.utils import load_cfg_from_registry, parse_env_cfg
 from omni.isaac.lab_tasks.utils.wrappers.rl_games import RlGamesGpuEnv, RlGamesVecEnvWrapper
 
